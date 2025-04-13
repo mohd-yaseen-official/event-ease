3b2aeab0583cb8119bcfa03288772cfbbe880ae8
Mohamed Yaseen <yaseenichu21@gmail.com>
2025-04-13 10:55:39 +0530

    Updated settings.py

--- src/event_ease/event_ease/settings.py
+++ src/event_ease/event_ease/settings.py
@@ -1,14 +1,16 @@
 from pathlib import Path
+from dotenv import load_dotenv
 import os
 
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv(BASE_DIR / '.env')
 
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 
-DEBUG = os.environ.get('DJANGO_DEBUG')
+DEBUG = os.environ.get('DJANGO_DEBUG') == 'True'
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
